cef0f0c47daf2f69d2523434e46fe2b80669e402
yaumul.majid <yaumulmajid@gmail.com>
2024-09-24 10:50:01 +0700

    add menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,34 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
-
 /*=============== SWIPER PROJECTS ===============*/
-
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
-
 /*=============== EMAIL JS ===============*/
-
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+/*=============== SHOW SCROLL UP ===============*/
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
