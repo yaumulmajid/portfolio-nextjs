a87b556867304f2b2fd952d925eef96ddbe7e8dc
yaumul.majid <yaumulmajid@gmail.com>
2024-09-25 18:25:12 +0700

    add skill

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,14 @@ if (navClose) {
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
 /*=============== SWIPER PROJECTS ===============*/
 
 /*=============== SWIPER TESTIMONIAL ===============*/
