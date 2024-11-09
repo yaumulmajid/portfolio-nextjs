588612900bfa97c3d6cae5904a442c98a4e27ada
yaumul.majid <yaumulmajid@gmail.com>
2024-11-09 17:37:38 +0700

    test

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,7 +17,7 @@ function ChevronRightIcon(props) {
 export function Card({ as: Component = 'div', className, children }) {
   return (
     <Component
-      className={clsx(className, 'group relative flex flex-col items-start')}
+      className={clsx(className, 'group  relative flex flex-col items-start')}
     >
       {children}
     </Component>
